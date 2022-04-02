88518c2c3f680acceb50fefddda1aa16c462bbe7
adarsh sharma <37371998+adarshsharma27@users.noreply.github.com>
2022-04-02 22:36:36 +0530

    Navigation Toggle fixes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { Link } from "react-scroll";
 
 const Navbar = () => {
+    const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+    const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
     return (
         <>
             <header>
@@ -11,37 +13,46 @@ const Navbar = () => {
                         <NavLink to="/" className="navbar-brand">
                             <h1 style={{ color: "#32B768" }}>{"Adarsh_Sharma_"}</h1>
                         </NavLink>
-                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <button
+                            className="navbar-toggler"
+                            type="button"
+                            data-toggle="collapse"
+                            data-target="#navbarSupportedContent"
+                            aria-controls="navbarSupportedContent"
+                            aria-expanded={!isNavCollapsed ? true : false}
+                            aria-label="Toggle navigation"
+                            onClick={handleNavCollapse}
+                        >
                             <span className="navbar-toggler-icon"></span>
                         </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <div className={`${isNavCollapsed ? "collapse" : ""} navbar-collapse`} id="navbarSupportedContent">
                             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                                 <NavLink to="/">
                                     <li className="nav-item">
-                                        <span className="nav-link active" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">
+                                        <span className="nav-link active" data-toggle="collapse" data-target="#navbarSupportedContent">
                                             Home
                                         </span>
                                     </li>
                                 </NavLink>
 
                                 <li className="nav-item">
-                                    <Link className="nav-link" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show" to="service" smooth={true}>
+                                    <Link className="nav-link" data-toggle="collapse" data-target="#navbarSupportedContent" to="service" smooth={true}>
                                         Services
                                     </Link>
                                 </li>
                                 <li className="nav-item">
-                                    <Link className="nav-link" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show" to="projects" smooth={true}>
+                                    <Link className="nav-link" data-toggle="collapse" data-target="#navbarSupportedContent" to="projects" smooth={true}>
                                         Work
                                     </Link>
                                 </li>
                                 <li className="nav-item">
-                                    <Link className="nav-link" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show" to="contactus" smooth={true}>
+                                    <Link className="nav-link" data-toggle="collapse" data-target="#navbarSupportedContent" to="contactus" smooth={true}>
                                         Contact Me
                                     </Link>
                                 </li>
                                 <NavLink to="/projects">
                                     <li className="nav-item">
-                                        <span className="nav-link" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">
+                                        <span className="nav-link" data-toggle="collapse" data-target="#navbarSupportedContent">
                                             Projects
                                         </span>
                                     </li>
